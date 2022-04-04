46a509068edf97d60e18adf6e45d8176015182fa
Hemalatha N <hema3021@global.chainsys.com>
2022-04-04 12:59:59 +0530

    files added

--- ng-workspace/demob-app/src/app/app.component.ts
+++ ng-workspace/demob-app/src/app/app.component.ts
@@ -1,13 +1,4 @@
-// import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'demob-app';
-// }
 
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
